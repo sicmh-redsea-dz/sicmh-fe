b8c886f4a3d7a4055ac239c41d2b7b7c6da1c909
zd77 <zavaladereck77@gmail.com>
2024-10-11 02:40:44 -0600

    This commits adds PandaCSS framework to the repo and adds the sidebar component.

--- src/app/dashboard/layouts/dashboard-layout/dashboard-layout.component.ts
+++ src/app/dashboard/layouts/dashboard-layout/dashboard-layout.component.ts
@@ -1,5 +1,6 @@
-import { Component, computed, inject } from '@angular/core';
-import { AuthService } from '../../../auth/services/auth.service';
+import { Component, computed, inject, OnInit } from '@angular/core'
+import { cva } from '../../../../../styled-system/css'
+import { AuthService } from '../../../auth/services/auth.service'
 
 @Component({
   selector: 'app-dashboard-layout',
@@ -7,10 +8,50 @@ import { AuthService } from '../../../auth/services/auth.service';
   styleUrl: './dashboard-layout.component.css'
 })
 export class DashboardLayoutComponent {
-  private authService = inject( AuthService );
-  public user = computed(() => this.authService.currentUser());
+  constructor(){
+    this.toggledStates = this.items.map(() => false);
+  }
+
+  public toggledStates:boolean[] = []
+  public items = [
+    {
+      label: 'Dashboard',
+      icon: 'ph-house-simple',
+      hasSubmenu: false,
+    },
+    {
+      label: 'Audience',
+      icon: 'ph-bold ph-user',
+      hasSubmenu: true,
+      subItems: ['Users', 'Subscribers'],
+      arrowIcon: 'ph-bold ph-caret-down'
+    },
+    {
+      label: 'Posts',
+      icon: 'ph-bold ph-file-text',
+      hasSubmenu: false,
+    },
+    {
+      label: 'Schedules',
+      icon: 'ph-bold ph-calendar-blank',
+      hasSubmenu: false,
+    },
+    {
+      label: 'Income',
+      icon: 'ph-bold ph-chart-bar',
+      hasSubmenu: true,
+      subItems: ['Earnings', 'Funds', 'Declines', 'Payouts'],
+      arrowIcon: 'ph-bold ph-caret-down'
+    }
+  ]
+  private authService = inject( AuthService )
+  public user = computed(() => this.authService.currentUser())
+
+  toggleItem( idx:number ) {
+    this.toggledStates = this.toggledStates.map((state, i) => i === idx ? !state : false);
+  }
 
   onLogout() {
-    this.authService.logout();
+    this.authService.logout()
   }
 }
