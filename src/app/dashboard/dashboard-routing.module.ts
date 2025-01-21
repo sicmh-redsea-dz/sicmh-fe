7d91b55976d72d76ba1ab791a8b082d22c880a8e
zd77 <zavaladereck77@gmail.com>
2025-01-21 17:37:29 -0600

    Fix for dashboard routing module.

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -43,7 +43,7 @@ const routes: Routes = [
       },
       { path: 'income/billings', component: BillingPageComponent },
       { path: 'settings', component: SettingsPageComponent },
-      { path: '**', redirectTo: 'income/billings'}
+      { path: '**', redirectTo: 'main'}
     ]
   },
 ];
